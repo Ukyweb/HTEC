f69fcb8df73a0eb03fd9813ee6158a7419657c93
Ukyweb <janicuros@gmail.com>
2017-08-05 10:38:28 +0200

    Add structure

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Place searches</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="js/main.js"></script>
+</head>
+<body>
+
+</body>
+</html>
