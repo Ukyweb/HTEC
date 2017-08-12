8622f6f9965a4cc9d5c59d7ee7af1fa88b689cdd
Ukyweb <janicuros@gmail.com>
2017-08-12 13:38:54 +0200

    Task 3
    adapted animation for speed limits and traffic lights

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// getting data from json
 function loadJSON(callback) {
 
     var xobj = new XMLHttpRequest();
@@ -14,16 +15,15 @@ function loadJSON(callback) {
     xobj.send(null);
 
 }
+
 var cars;
-// Call to function with anonymous callback
+
+// getting cars from json
 loadJSON(function(response) {
-    // Do Something with the response e.g.
+
     var jsonresponse = JSON.parse(response);
 
     cars = jsonresponse.cars;
-
-    //console.log(cars[0].name);
-
 
     var ul = document.getElementById("car_list");
     for ( var i=0; i<cars.length; i++ ) {
@@ -71,6 +71,7 @@ loadJSON(function(response) {
     raceTrack(jsonresponse);
 });
 
+// separating last row of car list
 function borderBottom() {
     var ul, li, i, liLeng, liLeftOver, liRound;
     ul = document.getElementById("car_list");
@@ -84,6 +85,7 @@ function borderBottom() {
     }
 }
 
+// filter car function
 function myFilter() {
     var input, inputV, filter, ul, li, a, i;
     input = document.getElementById("myInput");
@@ -107,7 +109,10 @@ function myFilter() {
         }
     }
 }
+
 var car1, car2, car3, divCar1, divCar2, divCar3, car1Speed, car2Speed, car3Speed;
+
+// choosing cars and adding image to race tracks
 function chooseCars(obj) {
     var value, id, divCar1div, divCar2div, divCar3div, url, info;
     value = obj.value;
@@ -116,7 +121,7 @@ function chooseCars(obj) {
     url = url[0].src;
     info = obj.getElementsByClassName("cars_info");
     info = info[0].getAttribute("value");
-    //console.log(info);
+
     divCar1 = document.getElementById("car1");
     divCar1div = divCar1.getElementsByTagName('div');
     divCar2 = document.getElementById("car2");
@@ -170,9 +175,11 @@ function chooseCars(obj) {
         }
     }
 }
-var raceT, raceTrackL;
+var raceT, raceTrackL, speedLimits, trafficLight, n1;
+
+// position of markers, speed limits and traffic lights
 function raceTrack(jsonresponse) {
-    var raceM, raceE, trackDistance, speedLimits, trafficLight, speedLimitsPos, trafficLightPos, trafficLightId, trLight, trTime, i, p, k, n1, n10;
+    var raceM, raceE, trackDistance, speedLimitsPos, trafficLightPos, trafficLightId, trLight, trTime, i, p, k, n10;
     raceT = document.getElementById("race_track");
     raceM = document.getElementById("race_markers");
     raceE = document.getElementById("race_events");
@@ -186,6 +193,7 @@ function raceTrack(jsonresponse) {
     n1 = raceTrackL/trackDistance;
     n10 = trackDistance/10;
 
+    // marker positions
     for ( i = 1; i < 10; i++) {
         raceM.innerHTML +=
             '<div class="distMarker" style="left: ' + n1*n10*i + 'px">'
@@ -194,6 +202,7 @@ function raceTrack(jsonresponse) {
             +'</div>';
     }
 
+    // position for speed limits
     for ( p = 0; p < speedLimits.length; p++) {
         speedLimitsPos = speedLimits[p].position;
         raceE.innerHTML +=
@@ -203,11 +212,12 @@ function raceTrack(jsonresponse) {
             + '</div>';
     }
 
+    // position and animation for traffic lights
     for ( k = 0; k < trafficLight.length; k++) {
         trafficLightPos = trafficLight[k].position;
         trafficLightId = "trLight" + k;
         raceE.innerHTML +=
-            '<div id="' + trafficLightId + '" class="traffic_light" style="left: ' + n1*trafficLightPos + 'px">'
+            '<div id="' + trafficLightId + '" class="traffic_light" style="left: ' + n1*trafficLightPos + 'px" value="0">'
                 + '<span></span>'
                 + '<div class="borderR">'
                     + '<span></span>'
@@ -222,14 +232,17 @@ function raceTrack(jsonresponse) {
         setInterval(function () {
             if (x == undefined) {
                 trLight.classList.add("green");
+                trLight.setAttribute("value", "1");
                 x = 1;
             } else if (x == 1) {
                 trLight.classList.remove("green");
+                trLight.setAttribute("value", "0");
                 x = undefined;
             }
         }, trTime);
     }
 }
+// cars race animation and traffic lights and speed limits
 function carRace() {
     var carLength, raceLength, left1, left2, left3, speed, speed1, speed2, speed3, places, slowest, animation;
 
@@ -256,7 +269,7 @@ function carRace() {
         left2 = 0;
         left3 = 0;
 
-
+        // sort chosen cars by speed
         places = [
             { name: 'car1', value: car1Speed },
             { name: 'car2', value: car2Speed },
@@ -266,8 +279,10 @@ function carRace() {
             return b.value - a.value;
         });
 
+        // slowest car
         slowest = places[2].name;
 
+        // add classes(medals) to cars by their speed (not counted for race)
         function medals(carName) {
             if (slowest == carName) {
                 document.getElementById(places[0].name).classList.add("gold");
@@ -278,32 +293,159 @@ function carRace() {
             }
         }
 
+        // position of first speed limit or traffic light
+        function min(s1) {
+            var arr = [];
+
+            for (var g = 0; g < speedLimits.length; g++) {
+                var maxPosL = speedLimits[g].position * n1;
+                var minPosL = maxPosL - s1 - carLength;
+
+                arr.push(minPosL);
+            }for (var h = 0; h < trafficLight.length; h++) {
+                var maxPosT = trafficLight[h].position * n1;
+                var minPosT = maxPosT - s1 - carLength;
+
+                arr.push(minPosT);
+            }
+            var minS = Math.min.apply(Math, arr);
+            return minS;
+        }
+        var min1 = min(speed1);
+        var min2 = min(speed2);
+        var min3 = min(speed3);
+
+        // car1 animation
         var travel1 = setInterval(move1, speed);
         function move1() {
+
             if (left1 >= raceLength) {
                 clearInterval(travel1);
                 medals("car1");
-            } else {
+            }
+
+            if (left1 <= min1) {
+                left1 += speed1;
+                divCar1.style.left = left1 + 'px';
+            }
+
+            //for traffic lights
+            for (var h = 0; h < trafficLight.length; h++) {
+                var maxPosST = trafficLight[h].position * n1 - carLength;
+                var minPosST = maxPosST - speed1;
+                var speedT = 0;
+                var lightId = "trLight" + h;
+                var lightV = document.getElementById(lightId).getAttribute("value");
+
+                if (left1 >= minPosST && left1 <= maxPosST && lightV == 0) {
+                    left1 += speedT;
+                    divCar1.style.left = left1 + 'px';
+                } else {
+
+                    // for speed Limits
+                    for (var j = 0; j < speedLimits.length; j++) {
+                        var maxPosSL = speedLimits[j].position * n1 - carLength;
+                        var minPosSL = maxPosSL - speed1;
+                        var speedL = speedLimits[j].speed/100;
+
+                        if (left1 > minPosSL) {
+                            left1 += speedL;
+                            divCar1.style.left = left1 + 'px';
+                        }
+                    }
+                }
+            }
+            if (speedLimits.length == 0 && trafficLight.length == 0) {
                 left1 += speed1;
                 divCar1.style.left = left1 + 'px';
             }
         }
+
+        //car2 animation
         var travel2 = setInterval(move2, speed);
         function move2() {
             if (left2 >= raceLength) {
                 clearInterval(travel2);
                 medals("car2");
-            } else {
+            }
+
+            if (left2 <= min2) {
+                left2 += speed2;
+                divCar2.style.left = left2 + 'px';
+            }
+
+            //for traffic lights
+            for (var h = 0; h < trafficLight.length; h++) {
+                var maxPosST = trafficLight[h].position * n1 - carLength;
+                var minPosST = maxPosST - speed2;
+                var speedT = 0;
+                var lightId = "trLight" + h;
+                var lightV = document.getElementById(lightId).getAttribute("value");
+
+                if (left2 >= minPosST && left2 <= maxPosST && lightV == 0) {
+                    left2 += speedT;
+                    divCar2.style.left = left2 + 'px';
+                } else {
+
+                    // for speed Limits
+                    for (var j = 0; j < speedLimits.length; j++) {
+                        var maxPosSL = speedLimits[j].position * n1 - carLength;
+                        var minPosSL = maxPosSL - speed2;
+                        var speedL = speedLimits[j].speed/100;
+
+                        if (left2 > minPosSL) {
+                            left2 += speedL;
+                            divCar2.style.left = left2 + 'px';
+                        }
+                    }
+                }
+            }
+            if (speedLimits.length == 0 && trafficLight.length == 0) {
                 left2 += speed2;
                 divCar2.style.left = left2 + 'px';
             }
         }
+
+        // car3 animation
         var travel3 = setInterval(move3, speed);
         function move3() {
             if (left3 >= raceLength) {
                 clearInterval(travel3);
                 medals("car3");
-            } else {
+            }
+
+            if (left3 <= min3) {
+                left3 += speed3;
+                divCar3.style.left = left3 + 'px';
+            }
+
+            //for traffic lights
+            for (var h = 0; h < trafficLight.length; h++) {
+                var maxPosST = trafficLight[h].position * n1 - carLength;
+                var minPosST = maxPosST - speed3;
+                var speedT = 0;
+                var lightId = "trLight" + h;
+                var lightV = document.getElementById(lightId).getAttribute("value");
+
+                if (left3 >= minPosST && left3 <= maxPosST && lightV == 0) {
+                    left3 += speedT;
+                    divCar3.style.left = left3 + 'px';
+                } else {
+
+                    // for speed Limits
+                    for (var j = 0; j < speedLimits.length; j++) {
+                        var maxPosSL = speedLimits[j].position * n1 - carLength;
+                        var minPosSL = maxPosSL - speed3;
+                        var speedL = speedLimits[j].speed/100;
+
+                        if (left3 > minPosSL) {
+                            left3 += speedL;
+                            divCar3.style.left = left3 + 'px';
+                        }
+                    }
+                }
+            }
+            if (speedLimits.length == 0 && trafficLight.length == 0) {
                 left3 += speed3;
                 divCar3.style.left = left3 + 'px';
             }
